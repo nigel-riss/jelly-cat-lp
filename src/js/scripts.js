69f3629dc86befde7562dd58be6ec7bab6070b0e
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-02-22 17:04:15 +0200

    Add contact popup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,4 +16,27 @@
     policyPopup.classList.add('policy-popup--hidden');
   });
 
+
+  // Contact popup window
+  var contactPopup = document.querySelector('.contact-popup');
+  var contactPopupClose = document.querySelector('.contact-popup__close');
+  var contactPopupCover = document.querySelector('.contact-popup__cover');
+
+  document.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('contact-button')) {
+      evt.preventDefault();
+      contactPopup.classList.remove('contact-popup--hidden');
+    }
+  });
+
+  contactPopupClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    contactPopup.classList.add('contact-popup--hidden');
+  });
+
+  contactPopupCover.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    contactPopup.classList.add('contact-popup--hidden');
+  });
+
 })();
